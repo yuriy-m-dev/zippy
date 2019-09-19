a20d28bc5fd01292440a73f775a9c38d789e81ff
yuriy-m-dev <fath66426@gmail.com>
2019-09-19 09:55:05 +0300

    added video projects

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,12 @@ $(function(){
         arrows: false,
         dots: true
     });
+
+    $('.video__inner').slick({
+        arrows: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        centerMode: true
+    });
 });
